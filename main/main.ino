1780565d6913b8c5b93720c44b461c39d8971f6e
captainspockears <ruthuparna1998@gmail.com>
2021-03-20 19:22:04 +0530

    Added comments to organise script development

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,34 @@
+/** @file     main.ino
+ *  
+ *  Code for the functioning of the seed counting machine.
+ *
+ *  @version  1.0.0
+ *  @author   captainspockears (Ruthuparna K)
+ */
+
+/*
+
+ ************* HARDWARE IMPLEMENTATION *************
+
+ */
+
+// WRITE HARDWARE CODE HERE
+ 
+
+/*
+
+ ************* SOFTWARE IMPLEMENTATION *************
+
+ */
+
+// WRITE SOFTWARE CODE HERE 
+
+/*
+
+ ************* MAIN IMPLEMENTATION *************
+
+ */
+
 void setup() {
   // put your setup code here, to run once:
 
